59215c0c40833932cb8f5b30e4b145d3b8530b5f
Tim-Van-den-Borre <72195069+Tim-Van-den-Borre@users.noreply.github.com>
2021-05-23 19:48:11 +0200

    Dashboard design

--- Website/resources/views/dashboard.blade.php
+++ Website/resources/views/dashboard.blade.php
@@ -1,40 +1,99 @@
 <x-app-layout>
     <x-slot name="header">
         <h3>{{ __('Dashboard') }}</h3>
+        <h5>
+            @if(Auth::user()->role == 'Doctor')
+                <p>Welcome Dr. {{ Auth::user()->name }}</p>
+            @elseif(Auth::user()->role == 'Patient')
+                <p>Welcome {{ Auth::user()->name }}</p>
+            @elseif(Auth::user()->role == 'Admin')
+                <p>Welcome Admin {{ Auth::user()->name }}</p>
+            @endif
+        </h5>
     </x-slot>
     <?php
         session_start();
         $_SESSION["userID"] = Auth::user()->id;
     ?>
-
-    <div class="py-12">
-        <div class="max-w-10xl mx-auto sm:px-8 lg:px-8">
-            <div class="mt-8 bg-gray dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                <div class="grid grid-cols-1 md:grid-cols-1">
-                    
-                    <div class="p-6" id="line">
-                        <div class="flex items-center">
-                            <div class="ml-4 text-lg leading-7 font-semibold"><p class="text-gray-900 dark:text-white">
-                                @if(Auth::user()->role == 'Doctor')
-                                    <p>Welcome Dr. {{ Auth::user()->name }}</p>
-                                @elseif(Auth::user()->role == 'Patient')
-                                    <p>Welcome {{ Auth::user()->name }}</p>
-                                @elseif(Auth::user()->role == 'Admin')
-                                    <p>Welcome Admin {{ Auth::user()->name }}</p>
-                                @endif
-                            </div>
-                        </div>
-                        @if(Auth::user()->role == 'Doctor')
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    <p>You have 12 appointments today. To view the appointments please navigate to the Calender tab.</p>
-                                    <p>10 patient(s) have assigned you as their doctor today. To view your patients please navigate to the Patients tab.</p>
-                                </div>
-                            </div>
-                        @endif
-                    </div>
+        <div class="row" style="margin: 0 auto;">
+            @if(Auth::user()->role == 'Admin')
+            <div class="card text-white bg-info mb-3" style="max-width: 18rem; margin: 0.5%;">
+                <div class="card-header">Patients</div>
+                <div class="card-body">
+                    <p class="card-text">Manage your patients and view their medical history.</p>
                 </div>
             </div>
+            <div class="card text-white bg-danger mb-3" style="max-width: 18rem; margin: 0.5%;">
+                <div class="card-header">Users</div>
+                <div class="card-body">
+                    <p class="card-text">Manage all users and their roles. Users can be removed / created here.</p>
+                </div>
+            </div>
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem; margin: 0.5%;">
+                <div class="card-header">Appointments</div>
+                <div class="card-body">
+                    <p class="card-text">Create your appointment with a doctor here.</p>
+                </div>
+            </div>
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem; margin: 0.5%;">
+                <div class="card-header">Calendar</div>
+                <div class="card-body">
+                    <p class="card-text">View your personal planning with appointments using your own personal calendar.</p>
+                </div>
+            </div>
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem; margin: 0.5%;">
+                <div class="card-header">Profile</div>
+                <div class="card-body">
+                    <p class="card-text">Make changes to your personal information, update password, download your data and remove your account here.</p>
+                </div>
+            </div>
+            @endif
+            @if(Auth::user()->role == 'Doctor')
+            <div class="card text-white bg-info mb-3" style="max-width: 18rem; margin: 0.5%;">
+                <div class="card-header">Patients</div>
+                <div class="card-body">
+                    <p class="card-text">Manage your patients and view their medical history.</p>
+                </div>
+            </div>
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem; margin: 0.5%;">
+                <div class="card-header">Appointments</div>
+                <div class="card-body">
+                    <p class="card-text">Create your appointment with a doctor here.</p>
+                </div>
+            </div>
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem; margin: 0.5%;">
+                <div class="card-header">Calendar</div>
+                <div class="card-body">
+                    <p class="card-text">View your personal planning with appointments using your own personal calendar.</p>
+                </div>
+            </div>
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem; margin: 0.5%;">
+                <div class="card-header">Profile</div>
+                <div class="card-body">
+                    <p class="card-text">Make changes to your personal information, update password, download your data and remove your account here.</p>
+                </div>
+            </div>
+            @endif
+            @if(Auth::user()->role == 'Patient')
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem; margin: 0.5%;">
+                <div class="card-header">Appointments</div>
+                <div class="card-body">
+                    <p class="card-text">Create your appointment with a doctor here.</p>
+                </div>
+            </div>
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem; margin: 0.5%;">
+                <div class="card-header">Calendar</div>
+                <div class="card-body">
+                    <p class="card-text">View your personal planning with appointments using your own personal calendar.</p>
+                </div>
+            </div>
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem; margin: 0.5%;">
+                <div class="card-header">Profile</div>
+                <div class="card-body">
+                    <p class="card-text">Make changes to your personal information, update password, download your data and remove your account here.</p>
+                </div>
+            </div>
+            @endif
         </div>
     </div>
 </x-app-layout>
